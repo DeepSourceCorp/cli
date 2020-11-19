5a25b91750e553761f08e856c36b8e7020b81f37
Aman Sharma <aman@deepsource.io>
2020-11-20 02:07:27 +0530

    fix: Working action tests for owner/detail module

--- test/store/owner/detail.spec.ts
+++ test/store/owner/detail.spec.ts
@@ -6,6 +6,8 @@ import { IssueTypeSetting, Owner } from '~/types/types';
 
 let actionCxt: OwnerModuleActionContext;
 let commit: jest.Mock;
+let localThis: any;
+let spy: any;
 let ownerState: OwnerModuleState;
 
 describe('[Store] Owner -- details module', () => {
@@ -26,11 +28,46 @@ describe('[Store] Owner -- details module', () => {
   describe('Actions', () => {
     describe(ACT_FETCH_ISSUE_TYPE_SETTINGS, () => {
       beforeEach(async () => {
+        localThis = {
+          $providerMetaMap: {
+            gh: {
+              text: 'Github',
+              shortcode: 'gh',
+              value: 'GITHUB'
+            }
+          },
+          async $fetchGraphqlData(): Promise<{ data: { owner: Owner } }> {
+            return new Promise<{ data: { owner: Owner } }>(resolve =>
+              setTimeout(() => resolve({ data: { owner: mockOwner().owner } }), 10)
+            );
+          }
+        }
 
+        // Setting the global spy on `localThis.$fetchGraphqlData`
+        spy = jest.spyOn(localThis, '$fetchGraphqlData')
+        
+        await (actions[ACT_FETCH_ISSUE_TYPE_SETTINGS] as Function).call(localThis, actionCxt, {
+          login: 'deepsourcelabs',
+          provider: 'gh'
+        })
       })
 
-      test('example', () => {
-        expect(true).toBe(true)
+      test('successfully calls the api', () => {
+        expect(spy).toHaveBeenCalledTimes(1);
+      })
+
+      test('successfully commits mutation', async () => {
+        expect(commit).toHaveBeenCalledTimes(1);
+
+        // Storing the first commit call made
+        const commitCall = commit.mock.calls[0];
+        const apiResponse = await localThis.$fetchGraphqlData()
+        
+        // Assert if `MUT_SET_ISSUE_TYPE_SETTING` is being commited or not.
+        expect(commitCall[0]).toEqual(MUT_SET_OWNER)
+
+        // Assert if the response from api is same as the one passed to the mutation.
+        expect(commitCall[1]).toEqual(apiResponse.data.owner)
       })
     })
   })
