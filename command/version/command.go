56387620da10407ede6e079a3d685db80509653d
Vishnu Jayadevan <vishnu@deepsource.io>
2021-05-27 13:56:47 +0530

    Minor refactoring for CLI auth
    Signed-off-by: Vishnu Jayadevan <vishnu@deepsource.io>

---
+++ command/version/command.go
@@ -0,0 +1,41 @@
+package version
+
+import (
+	"fmt"
+
+	"github.com/deepsourcelabs/cli/version"
+	"github.com/spf13/cobra"
+)
+
+// Options holds the metadata.
+type Options struct{}
+
+// For testing.  TODO: cleanup
+var getBuildInfo = version.GetBuildInfo
+
+// NewCmdVersion returns the current version of cli being used
+func NewCmdVersion() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "version",
+		Short: "Get the version of the DeepSource CLI",
+		Run: func(cmd *cobra.Command, args []string) {
+			o := Options{}
+			fmt.Println(o.Run())
+		},
+	}
+	return cmd
+}
+
+// Validate impletments the Validate method for the ICommand interface.
+func (Options) Validate() error {
+	return nil
+}
+
+// Run executest the command.
+func (Options) Run() string {
+	buildInfo := getBuildInfo()
+	if buildInfo == nil {
+		return ""
+	}
+	return getBuildInfo().String()
+}
