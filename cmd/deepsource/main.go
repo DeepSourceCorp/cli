475f33148b3d5264465ad6aacdd5cb5ca60a8f0f
siddhant-deepsource <siddhant@deepsource.io>
2021-07-05 23:32:13 +0530

    Remove legacy files and fetch sentry dsn from ldflag
    Signed-off-by: siddhant-deepsource <siddhant@deepsource.io>

--- cmd/deepsource/main.go
+++ cmd/deepsource/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/deepsourcelabs/cli/command"
 	"github.com/deepsourcelabs/cli/version"
+	"github.com/getsentry/sentry-go"
 	"github.com/pterm/pterm"
 )
 
@@ -20,12 +22,18 @@ var (
 func main() {
 
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	// code := mainRun()
-	// os.Exit(int(code))
+
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn: SentryDSN,
+	})
+	if err != nil {
+		fmt.Println("Could not load sentry.")
+	}
 
 	version.SetBuildInfo(Version, Date, "", "")
 
 	if err := command.Execute(); err != nil {
+		// TODO: Handle exit codes here
 		pterm.Error.Println(err)
 		os.Exit(1)
 	}
