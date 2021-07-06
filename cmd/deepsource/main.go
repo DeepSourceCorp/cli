fa7fd771779891870bf1c44c6a1f08c025cd2306
Siddhant N Trivedi <68370997+siddhant-deepsource@users.noreply.github.com>
2021-07-06 13:04:42 +0530

    Fix retrieval of sentry DSN (#59)
    * Remove legacy files and fetch sentry dsn from ldflag
    
    Signed-off-by: siddhant-deepsource <siddhant@deepsource.io>
    
    * Declare sentry dsn variable
    
    Signed-off-by: siddhant-deepsource <siddhant@deepsource.io>

--- cmd/deepsource/main.go
+++ cmd/deepsource/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/deepsourcelabs/cli/command"
 	"github.com/deepsourcelabs/cli/version"
+	"github.com/getsentry/sentry-go"
 	"github.com/pterm/pterm"
 )
 
@@ -15,17 +17,26 @@ var (
 
 	// Date is the build date.  This is set using ldflags -X
 	Date = "YYYY-MM-DD" // YYYY-MM-DD
+
+	// DSN used for sentry
+	SentryDSN string
 )
 
 func main() {
 
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	// code := mainRun()
-	// os.Exit(int(code))
+
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn: SentryDSN,
+	})
+	if err != nil {
+		fmt.Println("Could not load sentry.")
+	}
 
 	version.SetBuildInfo(Version, Date, "", "")
 
 	if err := command.Execute(); err != nil {
+		// TODO: Handle exit codes here
 		pterm.Error.Println(err)
 		os.Exit(1)
 	}
