3151f756f66fc80682214b2519e05afe94aaa3a9
Shivam Mishra <77610151+shivam-deepsource@users.noreply.github.com>
2021-09-13 12:34:40 +0530

    Revert "fix: localstore query in ssr (#615)" (#616)
    This reverts commit 51547b31f65f7fd29d3981175c2bebaca119fa2a.

--- components/Layout/Sidebar/MainSidebar.vue
+++ components/Layout/Sidebar/MainSidebar.vue
@@ -165,30 +165,19 @@ export default class Sidebar extends mixins(ContextMixin, ActiveUserMixin, RepoL
     this.fetchRepoCount()
   }
 
-  getPageSize(): number {
-    if (!process.client) {
-      return 10
-    }
-
-    return (
+  @Watch('activeOwner')
+  async fetchRepoCount(refetch?: boolean): Promise<void> {
+    const pageSize =
       (this.$localStore.get(
         `${this.activeProvider}-${this.activeProvider}-all-repos`,
         `currentPageSize`
       ) as number) || 10
-    )
-  }
-
-  @Watch('activeOwner')
-  async fetchRepoCount(refetch?: boolean): Promise<void> {
-    const pageSize = this.getPageSize()
-
     await this.fetchRepoList({
       provider: this.activeProvider,
       login: this.activeOwner,
       limit: pageSize,
       currentPageNumber: 1,
-      query: '',
-      refetch
+      query: ''
     })
   }
 
