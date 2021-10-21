a63f1151e52433616886b9139126284653eec54f
James George <james@deepsource.io>
2021-10-21 17:43:44 +0530

    feat: add loading state to `Activate new repo` modal [PLT-2994] (#791)
    * feat: add loading state to 'Activate new repo' modal
    
    * chore: update empty state
    
    Follow the Vue.js style guide

--- components/AddRepo/ActivateSingleRepo.vue
+++ components/AddRepo/ActivateSingleRepo.vue
@@ -1,5 +1,5 @@
 <template>
-  <z-tab-pane class="h-full flex flex-col">
+  <z-tab-pane class="flex flex-col h-full">
     <div class="p-4 pb-0 space-y-2">
       <z-input
         ref="search-repo-input"
@@ -11,47 +11,47 @@
         backgroundColor="ink-400"
         placeholder="Search repositories..."
         ><template slot="left">
-          <z-icon icon="search" size="small" class="ml-1.5"></z-icon>
+          <z-icon icon="search" size="small" class="ml-1.5" />
         </template>
       </z-input>
     </div>
-    <div class="flex-grow hide-scroll overflow-x-hidden">
-      <div class="space-y-2 p-4" v-if="newRepos.edges && newRepos.edges.length">
+    <div class="flex-grow overflow-x-hidden hide-scroll">
+      <div v-if="newRepos.edges && newRepos.edges.length" class="p-4 space-y-2">
         <repo-card
           v-for="repo in newRepos.edges"
           :key="repo.node.id"
           v-bind="repo.node"
-          class="hover:bg-ink-200 border rounded-lg border-ink-200"
+          class="border rounded-lg hover:bg-ink-200 border-ink-200"
           size="small"
           route="generate-config"
           :removeDefaultStyle="true"
           :showInfo="false"
           :analyzer-shortcode="analyzerShortcode"
           :transformer-shortcode="transformerShortcode"
-        ></repo-card>
+        />
       </div>
-      <div v-else-if="fetchingRepos" class="space-y-2 p-4 animate-pulse">
+      <div v-else-if="$fetchState.pending" class="p-4 space-y-2 animate-pulse">
         <div
           v-for="loopIndex in 4"
           :key="loopIndex"
-          class="h-17 rounded-md bg-ink-200 opacity-50"
+          class="rounded-md opacity-50 h-17 bg-ink-200"
         ></div>
       </div>
-      <div class="space-y-5 p-2 text-center flex flex-col items-center justify-center" v-else>
+      <div v-else class="flex flex-col items-center justify-center p-2 mt-10 space-y-5 text-center">
         <img
           v-if="searchCandidate"
           class="mx-auto"
           height="100px"
           width="auto"
-          src="~/assets/images/ui-states/onboard-empty-search.png"
-          alt="Searcing"
+          src="~/assets/images/ui-states/directory/empty-search.png"
+          :alt="searchCandidate"
         />
         <div>
           <template v-if="searchCandidate">
             We couldn’t find any matches for "{{ searchCandidate }}"
           </template>
           <template v-else> We couldn’t find any repositories linked to this account. </template>
-          <p class="text-sm text-vanilla-400 mt-1">
+          <p class="mt-1 text-sm text-vanilla-400">
             You can sync your repositories from VCS Provider
           </p>
         </div>
@@ -61,12 +61,7 @@
           class="flex items-center w-48"
           :disabled="true"
         >
-          <z-icon
-            icon="spin-loader"
-            class="animate-spin mr-2"
-            size="small"
-            color="ink-400"
-          ></z-icon>
+          <z-icon icon="spin-loader" class="mr-2 animate-spin" size="small" color="ink-400" />
           <span>Syncing repositories</span>
         </z-button>
         <z-button v-else size="small" class="w-48" icon="refresh-cw" @click="syncRepos">
@@ -112,9 +107,17 @@ export default class ActivateSingleRepo extends mixins(
   public searchCandidate = ''
   public pageSize = 20
   public repoSyncLoading = false
-  public fetchingRepos = false
 
-  async getData(): Promise<void> {
+  async searchRepo(val: string): Promise<void> {
+    this.searchCandidate = val
+    await this.$fetch()
+    const searchBox = this.$refs['search-repo-input'] as ZInputT
+    if (searchBox) {
+      searchBox.focus()
+    }
+  }
+
+  async fetch(): Promise<void> {
     await this.fetchNewRepoList({
       login: this.activeOwner,
       provider: this.activeProvider,
@@ -123,20 +126,9 @@ export default class ActivateSingleRepo extends mixins(
       query: this.searchCandidate ? this.searchCandidate : null
     })
     const searchBox = this.$refs['search-repo-input'] as ZInputT
-    if (searchBox) searchBox.focus()
-  }
-
-  searchRepo(val: string): void {
-    this.searchCandidate = val
-    this.getData()
-    const searchBox = this.$refs['search-repo-input'] as ZInputT
-    if (searchBox) searchBox.focus()
-  }
-
-  async fetch(): Promise<void> {
-    this.fetchingRepos = true
-    await this.getData()
-    this.fetchingRepos = false
+    if (searchBox) {
+      searchBox.focus()
+    }
   }
 
   async syncRepos(): Promise<void> {
@@ -149,7 +141,7 @@ export default class ActivateSingleRepo extends mixins(
 
     this.$socket.$on('repo-sync', async (data: { status: string }) => {
       if (data.status === 'success') {
-        await this.getData()
+        await this.$fetch()
         this.$toast.success('Repositories synced successfully.')
       } else if (data.status === 'failure') {
         this.$toast.danger('Error while syncing repositories. Please try again.')
