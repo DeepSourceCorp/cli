4c9bc4683d9404bd10b9604e17fcf5d94f4a4e9b
Shivam Mishra <77610151+shivam-deepsource@users.noreply.github.com>
2021-11-10 17:45:18 +0530

    fix: word break (#866)

--- components/RepoIssues/IssueDescription.vue
+++ components/RepoIssues/IssueDescription.vue
@@ -4,7 +4,7 @@
     <div class="px-4 text-sm font-medium tracking-wider uppercase text-vanilla-100">
       Description
     </div>
-    <div class="px-4 prose-sm prose text-vanilla-400">
+    <div class="px-4 prose-sm prose break-words text-vanilla-400">
       <p v-html="description"></p>
     </div>
   </div>
