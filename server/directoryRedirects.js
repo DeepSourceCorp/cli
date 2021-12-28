ad04f9afb5dd27ef52e8d4b07c48cc4fff24ac9a
Yash Dave <87606277+yash-deepsource@users.noreply.github.com>
2021-12-28 12:21:30 +0530

    feat:  add 301 for new directory [PLT-3092] (#955)
    * feat: add permanent redirect for `/analyzer/*` | `/transformer/*` routes
    
    * chore: add warning about 301s
    
    * fix: add deepsource suggestions

--- server/directoryRedirects.js
+++ server/directoryRedirects.js
@@ -1,5 +1,13 @@
-function redirect(res, location) {
-  res.writeHead(302, {
+/**
+ * A utility to perform a redirect to a different url
+ *
+ * @param  {Response} res
+ * @param  {string} location
+ * @param  {number} status
+ * @returns void
+ */
+function redirect(res, location, status = 302) {
+  res.writeHead(status, {
     location
   })
   res.end()
@@ -7,12 +15,20 @@ function redirect(res, location) {
 
 export default (req, res, next) => {
   const urlsToRedirect = ['/analyzers', '/transformers']
+  const urlRegexToPermanentlyRedirect = /(?<stringToReplace>\/analyzer\/|\/transformer\/)/i
   let { url } = req
   if (url.slice(-1) === '/') {
     url = url.slice(0, -1)
   }
   if (urlsToRedirect.includes(url)) {
     return redirect(res, '/directory')
+  }
+  if (urlRegexToPermanentlyRedirect.test(url)) {
+    const permanentlyRedirectedUrl = url
+      .replace(/\/analyzer\//i, '/analyzers/')
+      .replace(/\/transformer\//i, '/transformers/')
+    // ! A 301 redirect is permanent and often cached by browser, be sure to test it fully before deploying it.
+    return redirect(res, `/directory${permanentlyRedirectedUrl}`, 301)
   }
   return next()
 }
