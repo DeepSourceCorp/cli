8df07ddf72e44eca516f7e96d5666e734b4164bc
Aadhav Vignesh <53528139+burntcarrot@users.noreply.github.com>
2022-02-15 17:32:14 +0530

    fix(utils): add support for PAT based remote URLs (#90)
    * fix(utils): add support for PAT based remote URLs
    
    * add regular expressions to extract owner from remote URL
    * fix typos
    
    Signed-off-by: burntcarrot <aadhav.n1@gmail.com>
    
    * tests(utils): add tests for fetching remotes
    
    * add mocks for commands
    * add test for fetching remotes
    * refactor remote fetching utility to adjust mocks
    * refactor remote resolver
    
    Signed-off-by: burntcarrot <aadhav.n1@gmail.com>
    
    * refactor(utils): refactor tests and ListRemotes
    
    * refactor ListRemotes
    * refactor tests
    * add new test cases for SSH and multiple remotes
    * move to field-based splitting instead of tab-based splitting
    
    Signed-off-by: burntcarrot <aadhav.n1@gmail.com>
    
    * tests(utils): add new test case for simple HTTPS remotes
    
    Signed-off-by: burntcarrot <aadhav.n1@gmail.com>

---
+++ utils/fetch_remote_test.go
@@ -0,0 +1,81 @@
+package utils
+
+import (
+	"reflect"
+	"testing"
+)
+
+// Test data for PAT-based remote URLs
+var remotesPATMap = map[string][]string{
+	"origin": {"username", "repo", "GITHUB", "username/repo"},
+}
+var remotesPATList = []string{"origin	https://username:ghp_pat@github.com/username/repo (fetch)", "origin	https://username:ghp_pat@github.com/username/repo (push)"}
+
+// Test data for multiple remotes with PATs
+var multiRemotePATMap = map[string][]string{
+	"origin":   {"username", "repo", "GITHUB", "username/repo"},
+	"upstream": {"company", "repo", "GITHUB", "company/repo"},
+}
+var multiRemotePATList = []string{"origin	https://username:ghp_pat@github.com/username/repo (fetch)", "origin	https://username:ghp_pat@github.com/username/repo (push)", "upstream	https://username:ghp_pat@github.com/company/repo (fetch)", "upstream	https://username:ghp_pat@github.com/company/repo (push)"}
+
+// Test data for multiple remotes
+var multiRemoteMap = map[string][]string{
+	"origin":   {"username", "repo", "GITHUB", "username/repo"},
+	"upstream": {"company", "repo", "GITHUB", "company/repo"},
+}
+var multiRemoteList = []string{"origin	https://github.com/username/repo (fetch)", "origin	https://github.com/username/repo (push)", "upstream	https://github.com/company/repo (fetch)", "upstream	https://github.com/company/repo (push)"}
+
+// Test data for SSH URLs
+var sshRemoteMap = map[string][]string{
+	"origin":   {"username", "repo", "GITHUB", "username/repo"},
+	"upstream": {"company", "repo", "GITHUB", "company/repo"},
+}
+var sshRemoteList = []string{"origin	git@github.com:username/repo.git (fetch)", "origin	git@github.com:username/repo.git (push)", "upstream	git@github.com:company/repo.git (fetch)", "upstream	git@github.com:company/repo.git (push)"}
+
+func TestGetRemoteMap(t *testing.T) {
+	tests := []struct {
+		name    string
+		remotes []string
+		want    map[string][]string
+	}{
+		{
+			"remote URLs with PATs",
+			remotesPATList,
+			remotesPATMap,
+		},
+		{
+			"multiple remote URLs with PATs",
+			multiRemotePATList,
+			multiRemotePATMap,
+		},
+		{
+			"multiple remote URLs",
+			multiRemoteList,
+			multiRemoteMap,
+		},
+		{
+			"SSH URLs",
+			sshRemoteList,
+			sshRemoteMap,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			remoteMap, err := getRemoteMap(tt.remotes)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+
+			for remote := range remoteMap {
+				got := remoteMap[remote]
+				want := tt.want[remote]
+
+				if !reflect.DeepEqual(got, want) {
+					t.Errorf("got: %s; want: %s\n", got, want)
+				}
+			}
+		})
+	}
+}
