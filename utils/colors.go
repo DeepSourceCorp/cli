3710deae78c0df12c8531d16b9abd5af00190cf1
burntcarrot <aadhav.n1@gmail.com>
2022-03-07 13:56:57 +0530

    fix(utils): fix unpacking in color utility
    Signed-off-by: burntcarrot <aadhav.n1@gmail.com>

--- utils/colors.go
+++ utils/colors.go
@@ -4,10 +4,10 @@ import "github.com/fatih/color"
 
 func Yellow(format string, a ...interface{}) string {
 	c := color.New(color.FgYellow, color.Bold)
-	return c.Sprintf(format, a)
+	return c.Sprintf(format, a...)
 }
 
 func Cyan(format string, a ...interface{}) string {
 	c := color.New(color.FgCyan, color.Bold)
-	return c.Sprintf(format, a)
+	return c.Sprintf(format, a...)
 }
