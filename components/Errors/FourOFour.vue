ac7ed7877d1ac646d8f3943b6d0a4e38d42e9998
Shivam Mishra <77610151+shivam-deepsource@users.noreply.github.com>
2022-03-25 13:25:17 +0530

    feat: Duck norris 404 integration [PLT-4150] (#1125)
    * feat: add 404 page
    
    * feat: add responsive views to 404
    
    * feat: add 404
    
    * feat: integrate 404 in error page
    
    * revert: nuxt config
    
    * refactor: merge css
    
    * chore: update image
    
    * chore: remove button png
    
    * chore: remove unused css
    
    * chore: add doc strings
    
    * chore: update assets
    
    * chore: optimize images
    
    * chore: scope styles
    
    * refactor: remove alt
    
    * refactor: use png
    
    * chore: remove alt
    
    Co-authored-by: Yash Dave <87606277+yash-deepsource@users.noreply.github.com>
    
    Co-authored-by: Yash Dave <87606277+yash-deepsource@users.noreply.github.com>

---
+++ components/Errors/FourOFour.vue
@@ -0,0 +1,199 @@
+<template>
+  <div class="relative h-screen four-o-four">
+    <div class="absolute inset-0">
+      <div class="relative h-screen">
+        <picture class="absolute left-0 hidden h-screen xl:block">
+          <source
+            class="h-screen"
+            :srcset="require('~/assets/images/errors/404-bg-left.webp')"
+            type="image/webp"
+          />
+          <source
+            class="h-screen"
+            :srcset="require('~/assets/images/errors/404-bg-left.png')"
+            type="image/png"
+          />
+          <img :src="require('~/assets/images/errors/404-bg-left.png')" alt="" class="h-screen" />
+        </picture>
+        <picture class="absolute right-0 hidden h-screen xl:block">
+          <source
+            class="h-screen"
+            :srcset="require('~/assets/images/errors/404-bg-right.webp')"
+            type="image/webp"
+          />
+          <source
+            class="h-screen"
+            :srcset="require('~/assets/images/errors/404-bg-right.png')"
+            type="image/png"
+          />
+          <img :src="require('~/assets/images/errors/404-bg-right.png')" alt="" class="h-screen" />
+        </picture>
+        <picture class="absolute hidden top-28 left-40 nebula lg:block">
+          <source
+            class="h-48"
+            :srcset="require('~/assets/images/errors/nebula.webp')"
+            type="image/webp"
+          />
+          <source
+            class="h-48"
+            :srcset="require('~/assets/images/errors/nebula.png')"
+            type="image/png"
+          />
+          <img :src="require('~/assets/images/errors/nebula.png')" alt="" class="h-48" />
+        </picture>
+        <picture class="absolute top-15 right-15 rocket">
+          <source
+            class="h-20 md:h-28 lg:h-32"
+            :srcset="require('~/assets/images/errors/rocket.webp')"
+            type="image/webp"
+          />
+          <source
+            class="h-20 md:h-28 lg:h-32"
+            :srcset="require('~/assets/images/errors/rocket.png')"
+            type="image/png"
+          />
+          <img
+            :src="require('~/assets/images/errors/rocket.png')"
+            alt=""
+            class="h-20 md:h-28 lg:h-32"
+          />
+        </picture>
+      </div>
+    </div>
+    <div class="absolute bottom-0 w-full">
+      <div class="flex items-end">
+        <img
+          class="bottom-0 max-h-screen mx-auto left-10"
+          :src="require('~/assets/images/errors/moon.png')"
+          alt=""
+        />
+      </div>
+    </div>
+    <div class="absolute inset-0 bottom-0 flex flex-col justify-end w-full">
+      <div
+        class="flex flex-col items-center justify-between py-8 lg:justify-end h-2/3 lg:h-screen lg:space-y-12"
+      >
+        <div class="flex flex-col items-center justify-between text-center">
+          <span
+            class="text-2.5xl sm:text-3xl font-black tracking-widest text-vanilla-100 lg:text-ink-400"
+          >
+            404
+          </span>
+          <span
+            class="max-w-md text-1.5xl sm:text-2xl font-black text-center text-vanilla-100 lg:text-ink-400"
+          >
+            This dimension doesn't exist... yet!
+          </span>
+          <a href="/" class="home-button">Go home</a>
+        </div>
+        <span class="mx-auto text-vanilla-100 lg:text-ink-400">
+          © {{ new Date().getFullYear() }}, DeepSource Corp.
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+import { ZButton } from '@deepsourcelabs/zeal'
+
+/**
+ * 404 Error Page
+ */
+@Component({
+  components: { ZButton }
+})
+export default class FourOFour extends Vue {}
+</script>
+
+<style scoped>
+@font-face {
+  font-family: 'Retro Game';
+  src: local('Retro Game'), url(~/assets/fonts/retro-gaming.ttf) format('truetype');
+}
+
+.rocket {
+  animation: floating 5s steps(4, end) infinite;
+}
+
+.nebula {
+  animation: filter-animation 12s ease-in-out infinite;
+}
+
+.four-o-four {
+  background: #101116;
+  background-size: contain;
+  font-family: 'Retro Game';
+  background-image: url(~/assets/images/errors/stars.png);
+
+  cursor: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABFklEQVRYR9WXURLDIAhE6/0PbSdOtUpcd1Gnpv1KGpTHBpCE1/cXq+vrMph7dGvXZTtpfW10DCA5jrH1H0Jhs5E0hnZdCR+vb5S8Nn8mQCeS9BdSalYJqMBjAGzq59xAESN7VFVUgV8AZB/dZBR7QTFDCqGquvUBVVoEtgIwpQRzmANSFHgWQKExHdIrPeuMvQNDarXe6nC/AutgV3JW+6bgqQLeV8FekRtgV+ToDKEKnACYKsfZjjkam7a0ZpYTytwmgainpC3HvwBocgKOxqRjehoR9DFKNFYtOwCGYCszobeCbl26N6yyQ6g8X/Wex/rBPsNEV6qAMaJPMynIHQCoSqS9JSMmwef51LflTgCRszU7DvAGiV6mHWfsaVUAAAAASUVORK5CYII=),
+    auto;
+}
+
+.home-button::after {
+  @apply absolute;
+
+  top: -4px;
+  right: -4px;
+  bottom: -4px;
+  left: -4px;
+  content: '';
+  box-shadow: inset -5px -5px #2eb78b;
+}
+
+.home-button {
+  @apply bg-juniper text-ink-400 px-4 py-1.5 text-xl mt-8 border-solid border-4 relative inline-block text-center select-none;
+
+  border-image-slice: 2;
+  border-image-width: 2;
+  border-image-repeat: stretch;
+  border-image-source: url('data:image/svg+xml;utf8,<?xml version="1.0" encoding="UTF-8" ?><svg version="1.1" width="5" height="5" xmlns="http://www.w3.org/2000/svg"><path d="M2 1 h1 v1 h-1 z M1 2 h1 v1 h-1 z M3 2 h1 v1 h-1 z M2 3 h1 v1 h-1 z" fill="rgb(22, 24, 29)" /></svg>');
+  border-image-outset: 2;
+  border-image-repeat: space;
+  cursor: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAzElEQVRYR+2X0Q6AIAhF5f8/2jYXZkwEjNSVvVUjDpcrGgT7FUkI2D9xRfQETwNIiWO85wfINfQUEyxBG2ArsLwC0jioGt5zFcwF4OYDPi/mBYKm4t0U8ATgRm3ThFoAqkhNgWkA0jJLvaOVSs7j3qMnSgXWBMiWPXe94QqMBMBc1VZIvaTu5u5pQewq0EqNZvIEMCmxAawK0DNkay9QmfFNAJUXfgGgUkLaE7j/h8fnASkxHTz0DGIBMCnBeeM7AArpUd3mz2x3C7wADglA8BcWMZhZAAAAAElFTkSuQmCC)
+      14 0,
+    pointer;
+}
+
+.home-button:focus {
+  box-shadow: 0 0 0 6px rgba(#2eb78b, 0.3);
+}
+
+.home-button:active::after {
+  box-shadow: inset 5px 5px #2eb78b;
+  @apply border-juniper-600;
+}
+
+@keyframes floating {
+  0% {
+    transform: translateY(0px);
+  }
+  25% {
+    transform: translateY(10px);
+  }
+  50% {
+    transform: translateY(0px);
+  }
+  75% {
+    transform: translateY(-10px);
+  }
+  100% {
+    transform: translateY(0px);
+  }
+}
+
+@keyframes filter-animation {
+  0% {
+    filter: hue-rotate(0deg);
+  }
+
+  50% {
+    filter: hue-rotate(100deg);
+  }
+
+  100% {
+    filter: hue-rotate(0deg);
+  }
+}
+</style>
--- layouts/error.vue
+++ layouts/error.vue
@@ -1,103 +1,102 @@
 <template>
-  <div class="flex items-center justify-center min-h-screen bg-ink-400 text-vanilla-100">
-    <section
-      v-if="$config.onPrem && error.statusCode === 403 && error.message === 'license-expired'"
-      class="max-w-xl space-y-8 text-center"
-    >
-      <img
-        class="w-56 max-w-xs mx-auto mb-8"
-        :src="require('~/assets/images/ui-states/app/license-expired.png')"
-        alt="Repo Inactive"
-      />
-      <div class="space-y-4">
-        <h3 class="text-lg font-semibold text-center text-vanilla-100">
-          Your DeepSource license has expired.
-        </h3>
-        <p class="text-sm text-vanilla-400">
-          The license for this installation has expired as of
-          <b class="text-vanilla-100"> {{ formatDate($config.licenseExpiry) }}. </b>
-          <br />Please contact your DeepSource installation administrator.
-        </p>
-      </div>
-      <div class="flex items-center justify-center space-x-3">
-        <z-button size="small" :to="`mailto:${$config.supportEmail}`" icon="support">
-          Contact support
-        </z-button>
-      </div>
-    </section>
-    <section v-else-if="$config.onPrem && areSeatsFull" class="max-w-xl space-y-8 text-center">
-      <img
-        class="w-56 max-w-xs mx-auto mb-8"
-        :src="require('~/assets/images/ui-states/app/license-expired.png')"
-        alt="Repo Inactive"
-      />
-      <div class="space-y-4">
-        <h3 class="text-lg font-semibold text-center text-vanilla-100">
-          You have exhausted all seats in your DeepSource license.
-        </h3>
-        <p class="text-sm text-vanilla-400">
-          Please contact your administrator for upgrading the license.
-        </p>
-      </div>
-      <div class="flex items-center justify-center space-x-3">
-        <z-button size="small" :to="`mailto:${$config.supportEmail}`" icon="support">
-          Contact support
-        </z-button>
-      </div>
-    </section>
-    <div
-      v-else-if="[500, 501, 502, 503, 504].includes(error.statusCode)"
-      class="max-w-xl space-y-5 text-center"
-    >
-      <!-- 
+  <main>
+    <four-o-four v-if="error.statusCode === 404" />
+    <div v-else class="flex items-center justify-center min-h-screen bg-ink-400 text-vanilla-100">
+      <section
+        v-if="$config.onPrem && error.statusCode === 403 && error.message === 'license-expired'"
+        class="max-w-xl space-y-8 text-center"
+      >
+        <img
+          class="w-56 max-w-xs mx-auto mb-8"
+          :src="require('~/assets/images/ui-states/app/license-expired.png')"
+          alt="Repo Inactive"
+        />
+        <div class="space-y-4">
+          <h3 class="text-lg font-semibold text-center text-vanilla-100">
+            Your DeepSource license has expired.
+          </h3>
+          <p class="text-sm text-vanilla-400">
+            The license for this installation has expired as of
+            <b class="text-vanilla-100"> {{ formatDate($config.licenseExpiry) }}. </b>
+            <br />Please contact your DeepSource installation administrator.
+          </p>
+        </div>
+        <div class="flex items-center justify-center space-x-3">
+          <z-button size="small" :to="`mailto:${$config.supportEmail}`" icon="support">
+            Contact support
+          </z-button>
+        </div>
+      </section>
+      <section v-else-if="$config.onPrem && areSeatsFull" class="max-w-xl space-y-8 text-center">
+        <img
+          class="w-56 max-w-xs mx-auto mb-8"
+          :src="require('~/assets/images/ui-states/app/license-expired.png')"
+          alt="Repo Inactive"
+        />
+        <div class="space-y-4">
+          <h3 class="text-lg font-semibold text-center text-vanilla-100">
+            You have exhausted all seats in your DeepSource license.
+          </h3>
+          <p class="text-sm text-vanilla-400">
+            Please contact your administrator for upgrading the license.
+          </p>
+        </div>
+        <div class="flex items-center justify-center space-x-3">
+          <z-button size="small" :to="`mailto:${$config.supportEmail}`" icon="support">
+            Contact support
+          </z-button>
+        </div>
+      </section>
+      <div
+        v-else-if="[500, 501, 502, 503, 504].includes(error.statusCode)"
+        class="max-w-xl space-y-5 text-center"
+      >
+        <!-- 
         500 Internal Server Error
         501 Not Implemented
         502 Bad Gateway
         503 Service Unavailable
         504 Gateway Timeout 
       -->
-      <h1 class="font-black text-center text-7xl">{{ error.statusCode }}</h1>
-      <div class="space-y-2">
-        <p class="text-lg font-semibold text-center text-vanilla-100">
-          There was an error while accessing DeepSource.
-        </p>
-        <p class="max-w-md mx-auto text-sm text-vanilla-200">
-          Our engineers are working to fix this, if this issue persists, please contact DeepSource
-          support. We regret any inconvenience caused.
-        </p>
+        <h1 class="font-black text-center text-7xl">{{ error.statusCode }}</h1>
+        <div class="space-y-2">
+          <p class="text-lg font-semibold text-center text-vanilla-100">
+            There was an error while accessing DeepSource.
+          </p>
+          <p class="max-w-md mx-auto text-sm text-vanilla-200">
+            Our engineers are working to fix this, if this issue persists, please contact DeepSource
+            support. We regret any inconvenience caused.
+          </p>
+        </div>
+        <div class="flex items-center justify-center space-x-3">
+          <a
+            v-if="!$config.onPrem"
+            href="https://deepsourcestatus.com/"
+            target="blank"
+            rel="noreferrer noopener"
+            class="text-sm text-center text-juniper hover:underline"
+          >
+            Status page
+          </a>
+          <a
+            v-if="$config.supportEmail"
+            class="text-sm text-center text-juniper hover:underline"
+            :href="`mailto:${$config.supportEmail}`"
+          >
+            Contact support
+          </a>
+        </div>
       </div>
-      <div class="flex items-center justify-center space-x-3">
-        <a
-          v-if="!$config.onPrem"
-          href="https://deepsourcestatus.com/"
-          target="blank"
-          rel="noreferrer noopener"
-          class="text-sm text-center text-juniper hover:underline"
-        >
-          Status page
-        </a>
-        <a
-          v-if="$config.supportEmail"
-          class="text-sm text-center text-juniper hover:underline"
-          :href="`mailto:${$config.supportEmail}`"
-        >
-          Contact support
-        </a>
-      </div>
-    </div>
-    <div v-else class="max-w-xl space-y-5 text-center">
-      <h1 class="font-black text-center text-7xl">{{ error.statusCode }}</h1>
-      <p class="text-lg font-semibold text-center text-vanilla-100">{{ error.message }}</p>
-      <p v-if="error.statusCode === 404" class="text-vanilla-400">
-        Just like the theory that says '404' was named after a room at CERN where the first web
-        servers were located.
-      </p>
-      <div class="flex items-center justify-center space-x-3">
-        <a href="/" class="text-sm text-center text-juniper hover:underline"> Home page </a>
-        <a class="text-sm text-center text-juniper hover:underline" :href="loginUrl">Login</a>
+      <div v-else class="max-w-xl space-y-5 text-center">
+        <h1 class="font-black text-center text-7xl">{{ error.statusCode }}</h1>
+        <p class="text-lg font-semibold text-center text-vanilla-100">{{ error.message }}</p>
+        <div class="flex items-center justify-center space-x-3">
+          <a href="/" class="text-sm text-center text-juniper hover:underline"> Home page </a>
+          <a class="text-sm text-center text-juniper hover:underline" :href="loginUrl">Login</a>
+        </div>
       </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <script lang="ts">
