64c390407b648a4ed6017f6af5f23a6485ef77ae
Aadhav Vignesh <53528139+burntcarrot@users.noreply.github.com>
2022-04-13 16:11:14 +0530

    tests(utils): add tests for remote resolver (#127)
    * tests(utils): add tests for remote resolver
    
    Signed-off-by: burntcarrot <aadhav.n1@gmail.com>
    
    * tests(utils): add tests for remote URLs having short forms
    
    Signed-off-by: burntcarrot <aadhav.n1@gmail.com>

---
+++ utils/remote_resolver_test.go
@@ -0,0 +1,76 @@
+package utils
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestResolveRemote(t *testing.T) {
+	tests := []struct {
+		name    string
+		repoArg string
+		want    *RemoteData
+		wantErr bool
+	}{
+		{
+			name:    "valid github remote URL",
+			repoArg: "github.com/deepsourcelabs/cli",
+			want:    &RemoteData{Owner: "deepsourcelabs", RepoName: "cli", VCSProvider: "GITHUB"},
+			wantErr: false,
+		},
+		{
+			name:    "valid github remote URL (short form)",
+			repoArg: "gh/deepsourcelabs/cli",
+			want:    &RemoteData{Owner: "deepsourcelabs", RepoName: "cli", VCSProvider: "GITHUB"},
+			wantErr: false,
+		},
+		{
+			name:    "valid gitlab remote URL",
+			repoArg: "gitlab.com/deepsourcelabs/cli",
+			want:    &RemoteData{Owner: "deepsourcelabs", RepoName: "cli", VCSProvider: "GITLAB"},
+			wantErr: false,
+		},
+		{
+			name:    "valid gitlab remote URL (short form)",
+			repoArg: "gl/deepsourcelabs/cli",
+			want:    &RemoteData{Owner: "deepsourcelabs", RepoName: "cli", VCSProvider: "GITLAB"},
+			wantErr: false,
+		},
+		{
+			name:    "valid bitbucket remote URL",
+			repoArg: "bitbucket.com/deepsourcelabs/cli",
+			want:    &RemoteData{Owner: "deepsourcelabs", RepoName: "cli", VCSProvider: "BITBUCKET"},
+			wantErr: false,
+		},
+		{
+			name:    "valid bitbucket remote URL (short form)",
+			repoArg: "bb/deepsourcelabs/cli",
+			want:    &RemoteData{Owner: "deepsourcelabs", RepoName: "cli", VCSProvider: "BITBUCKET"},
+			wantErr: false,
+		},
+		{
+			name:    "invalid VCS provider",
+			repoArg: "example.com/deepsourcelabs/cli",
+			want:    nil,
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ResolveRemote(tt.repoArg)
+
+			if tt.wantErr {
+				assert.NotNil(t, err)
+			} else {
+				assert.Nil(t, err)
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("got: %v, want: %v\n", got, tt.want)
+			}
+		})
+	}
+}
