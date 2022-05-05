3358e48a2043c194ba3942972baa15785304edc1
Siddhant N Trivedi <siddhant@deepsource.io>
2022-05-05 16:12:21 +0530

    chore: add test for validation analyzer.toml and add new analyzer TOML schema
    Signed-off-by: Siddhant N Trivedi <siddhant@deepsource.io>

--- macros/validator/validator.go
+++ macros/validator/validator.go
@@ -8,29 +8,14 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"reflect"
 
 	validate "github.com/go-playground/validator/v10"
 	"github.com/spf13/viper"
 )
 
-var (
-	configFolder     string
-	analyzerTOMLPath string
-	issuesDirPath    string
-)
-
 // Checks if the analyzer.toml file and the issue directory is present
-func CheckConfigPresent() (err error) {
-	// Check if `.deepsource/analyzer` folder is present
-	cwd, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-	configFolder = filepath.Join(cwd, ".deepsource/analyzer")
-
-	analyzerTOMLPath = filepath.Join(configFolder, "analyzer.toml")
-	issuesDirPath = filepath.Join(configFolder, "issues/")
-
+func CheckForAnalyzerConfig(analyzerTOMLPath, issuesDirectoryPath string) (err error) {
 	// Check if `analyzer.toml` is present in `.deepsource/analyzer` folder
 	if _, err := os.Stat(analyzerTOMLPath); err != nil {
 		if errors.Is(err, os.ErrNotExist) {
@@ -39,7 +24,7 @@ func CheckConfigPresent() (err error) {
 	}
 
 	// Check if `issues/` directory is present in `.deepsource/analyzer` folder and is not empty.
-	if _, err := os.Stat(issuesDirPath); err != nil {
+	if _, err := os.Stat(issuesDirectoryPath); err != nil {
 		if errors.Is(err, os.ErrNotExist) {
 			return errors.New("the issue descriptions directory doesn't exist")
 		}
@@ -48,33 +33,45 @@ func CheckConfigPresent() (err error) {
 }
 
 // Validates analyzer.toml file
-func ValidateAnalyzerTOML() (err error) {
+func ValidateAnalyzerTOML(analyzerTOMLPath string) (analyzerConfig AnalyzerMetadata, err error) {
+	config := AnalyzerMetadata{}
 	// Read the contents of analyzer.toml file
 	analyzerTOMLContent, err := ioutil.ReadFile(analyzerTOMLPath)
 	if err != nil {
-		return errors.New("failed to read analyzer.toml file")
+		return config, errors.New("failed to read analyzer.toml file")
 	}
 
-	config := AnalyzerMetadata{}
 	viper.SetConfigType("toml")
 	if err = viper.ReadConfig(bytes.NewBuffer(analyzerTOMLContent)); err != nil {
-		return err
+		return config, err
 	}
 	// Unmarshaling the configdata into AnalyzerMetadata struct
 	viper.Unmarshal(&config)
 
 	// Validate analyzer.toml fields based on type and sanity checks
 	v := validate.New()
+	missingRequiredFields := []string{}
 	if err := v.Struct(&config); err != nil {
-		log.Fatalf("Missing required attributes %v\n", err)
+		// Improve error message returned by `go-playground/validator`
+		errs := err.(validate.ValidationErrors)
+		for _, err := range errs {
+			if err.Tag() == "required" {
+				c := reflect.ValueOf(config)
+				for i := 0; i < c.Type().NumField(); i++ {
+					if err.Field() == c.Type().Field(i).Name {
+						missingRequiredFields = append(missingRequiredFields, c.Type().Field(i).Tag.Get("toml"))
+					}
+				}
+			}
+		}
+		return config, fmt.Errorf("missing required attributes %v\n", missingRequiredFields)
 	}
-
-	return nil
+	return config, nil
 }
 
 // Validates issue description TOML files
-func ValidateIssueDescriptions() (err error) {
-	issuesList, err := ioutil.ReadDir(issuesDirPath)
+func ValidateIssueDescriptions(issuesDirectoryPath string) (err error) {
+	issuesList, err := ioutil.ReadDir(issuesDirectoryPath)
 	if err != nil {
 		return err
 	}
@@ -82,7 +79,7 @@ func ValidateIssueDescriptions() (err error) {
 	config := AnalyzerIssue{}
 	for _, issuePath := range issuesList {
 		// Read the contents of issue toml file
-		issueTOMLContent, err := ioutil.ReadFile(filepath.Join(issuesDirPath, issuePath.Name()))
+		issueTOMLContent, err := ioutil.ReadFile(filepath.Join(issuesDirectoryPath, issuePath.Name()))
 		if err != nil {
 			fmt.Println(err)
 			return errors.New("Error occured while reading file:%s. Exiting...")
@@ -101,6 +98,5 @@ func ValidateIssueDescriptions() (err error) {
 		}
 		config = AnalyzerIssue{}
 	}
-
 	return nil
 }
