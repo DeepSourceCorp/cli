97e6113413bc4517cbca30742b8609cd24d1e4ee
Siddhant N Trivedi <siddhant@deepsource.io>
2022-05-06 16:00:43 +0530

    chore: add spinner utility
    Signed-off-by: Siddhant N Trivedi <siddhant@deepsource.io>

---
+++ utils/spinner.go
@@ -0,0 +1,37 @@
+package utils
+
+import (
+	"os"
+	"sync"
+	"time"
+
+	"github.com/briandowns/spinner"
+)
+
+type SpinnerUtils struct {
+	mu      sync.Mutex
+	Spinner *spinner.Spinner
+}
+
+func (s *SpinnerUtils) StartSpinnerWithLabel(label string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	sp := spinner.New(spinner.CharSets[11], 120*time.Millisecond, spinner.WithWriter(os.Stderr)) // Build our new spinner
+	if label != "" {
+		sp.Suffix = " "+ label + " "
+	}
+	sp.Start() // Start the spinner
+	s.Spinner = sp
+}
+
+func (s *SpinnerUtils) StopSpinner() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if s.Spinner == nil {
+		return
+	}
+	s.Spinner.Stop()
+	s.Spinner = nil
+}
