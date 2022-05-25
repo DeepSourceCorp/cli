7ba3fbecc873e017df4ac90dbda31d66df9c9382
Siddhant N Trivedi <siddhant@deepsource.io>
2022-05-25 14:31:51 +0530

    chore: fix DS issue
    Signed-off-by: Siddhant N Trivedi <siddhant@deepsource.io>

--- command/analyzer/run/result.go
+++ command/analyzer/run/result.go
@@ -1,29 +1,13 @@
 package run
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
 	"path"
 	"strings"
-
-	analysis_config "github.com/deepsourcelabs/cli/analysis/config"
 )
 
-// Reads the LSP based analysis result written in the `analysis_results.json` file present in
-// the directory of the Analyzer
-func (*AnalyzerRunOpts) processAnalysisResult() (err error) {
-	analysisResultsData, err := os.ReadFile("analysis_results.json")
-	if err != nil {
-		return err
-	}
-
-	// Unmarshal the analysis result data present in the LSP format
-	analysisResultContent := analysis_config.AnalysisResult{}
-	return json.Unmarshal(analysisResultsData, &analysisResultContent)
-}
-
-// Writes the analysis results into a file with the name `fileName`
+// Writes the analysis results into a file with the name specified in the `fileName` parameter
 func (a *AnalyzerRunOpts) writeAnalysisResults(buf []byte, fileName string) (err error) {
 	// Ref: https://deepsource.io/directory/analyzers/go/issues/GSC-G305
 	if !strings.Contains(string(buf), "..") {
