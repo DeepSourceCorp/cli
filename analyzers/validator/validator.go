5017045b2e6db258678418a62132e620aaf36c42
Siddhant N Trivedi <68370997+siddhant-deepsource@users.noreply.github.com>
2022-06-15 16:27:20 +0530

    Update analyzers/validator/validator.go
    Co-authored-by: Sourya Vatsyayan <sourya@deepsource.io>

--- analyzers/validator/validator.go
+++ analyzers/validator/validator.go
@@ -116,7 +116,6 @@ func ValidateAnalyzerTOML(analyzerTOMLPath string) (*types.AnalyzerTOML, *Valida
 func ValidateIssueDescriptions(issuesDirectoryPath string) (*[]ValidationFailure, error) {
 	issueValidationErrors := []ValidationFailure{}
 
-	// skipcq: I001
 	// TODO: List only TOML files here
 	issuesList, err := ioutil.ReadDir(issuesDirectoryPath)
 	if err != nil {
