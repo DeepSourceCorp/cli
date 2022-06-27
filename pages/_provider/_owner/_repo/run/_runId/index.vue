89d29963008794a2bd97956bf1dfe35fb38e15b1
Shivam Mishra <77610151+shivam-deepsource@users.noreply.github.com>
2022-06-27 22:59:52 +0530

    fix: middleware (#1311)

--- pages/_provider/_owner/_repo/run/_runId/index.vue
+++ pages/_provider/_owner/_repo/run/_runId/index.vue
@@ -6,19 +6,23 @@ import { RunDetailActions } from '@/store/run/detail'
 
 @Component({
   middleware: [
-    async function ({ store, route, redirect }) {
-      const { provider, owner, repo, runId } = route.params
-      await store.dispatch(`run/detail/${RunDetailActions.FETCH_RUN}`, {
-        provider,
-        owner,
-        name: repo,
-        runId
-      })
-      const { checks } = store.state.run.detail.run
+    async function ({ store, route, error, redirect }) {
+      try {
+        const { provider, owner, repo, runId } = route.params
+        await store.dispatch(`run/detail/${RunDetailActions.FETCH_RUN}`, {
+          provider,
+          owner,
+          name: repo,
+          runId
+        })
+        const { checks } = store.state.run.detail.run
 
-      if (checks?.edges) {
-        const code = checks.edges[0].node.analyzer.shortcode
-        redirect(302, `${stripTrailingSlash(route.path)}/${code}`)
+        if (checks?.edges) {
+          const code = checks.edges[0].node.analyzer.shortcode
+          redirect(302, `${stripTrailingSlash(route.path)}/${code}`)
+        }
+      } catch {
+        error({ statusCode: 404, message: 'This page is not real' })
       }
     }
   ],
