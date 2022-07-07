0b031b33a29f1a2ad047d838a4c8bc59daaa254c
burntcarrot <aadhav.n1@gmail.com>
2022-07-07 08:32:19 +0530

    feat: add plugin support

--- command/root.go
+++ command/root.go
@@ -1,7 +1,9 @@
 package command
 
 import (
-	"github.com/deepsourcelabs/cli/command/analyzer"
+	"log"
+	"plugin"
+
 	"github.com/deepsourcelabs/cli/command/auth"
 	"github.com/deepsourcelabs/cli/command/config"
 	"github.com/deepsourcelabs/cli/command/issues"
@@ -10,6 +12,8 @@ import (
 	"github.com/deepsourcelabs/cli/command/version"
 	"github.com/spf13/cobra"
 )
+
+var commands []*cobra.Command
 
 func NewCmdRoot() *cobra.Command {
 	cmd := &cobra.Command{
@@ -23,16 +27,39 @@ Login into DeepSource using the command : deepsource auth login`,
 		SilenceUsage:  true,
 	}
 
+	analyzerCmd := GetCmd("deepsource.so", "NewCmdAnalyzer")
+
 	// Child Commands
-	cmd.AddCommand(version.NewCmdVersion())
-	cmd.AddCommand(config.NewCmdConfig())
-	cmd.AddCommand(auth.NewCmdAuth())
-	cmd.AddCommand(repo.NewCmdRepo())
-	cmd.AddCommand(issues.NewCmdIssues())
-	cmd.AddCommand(report.NewCmdReport())
-	cmd.AddCommand(analyzer.NewCmdAnalyzer())
+	commands = append(commands, version.NewCmdVersion(), config.NewCmdConfig(), auth.NewCmdAuth(), repo.NewCmdRepo(), issues.NewCmdIssues(), report.NewCmdReport())
+
+	if analyzerCmd != nil {
+		log.Println("couldn't find analyzer cmd")
+		commands = append(commands, analyzerCmd)
+	}
+
+	cmd.AddCommand(commands...)
 
 	return cmd
+}
+
+func GetCmd(pluginPath, cmdName string) *cobra.Command {
+	p, err := plugin.Open(pluginPath)
+	if err != nil {
+		return nil
+	}
+	b, err := p.Lookup(cmdName)
+	log.Printf("errb: %v\n", err)
+	log.Printf("b: %v\n", b)
+	if err != nil {
+		return nil
+	}
+	f, err := p.Lookup(cmdName)
+	log.Printf("f: %v\n", f)
+	if err == nil {
+		f.(func())()
+	}
+
+	return *b.(**cobra.Command)
 }
 
 func Execute() error {
