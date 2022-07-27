6162c56f249c6d472ded7cf8b289891622fa173f
burntcarrot <aadhav.n1@gmail.com>
2022-07-27 16:38:40 +0530

    feat: add DeepSource TCV reporter

---
+++ cmd/deepsource_tcv/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	commandtcv "github.com/deepsourcelabs/cli/command_tcv"
+	v "github.com/deepsourcelabs/cli/version"
+	"github.com/getsentry/sentry-go"
+	"github.com/pterm/pterm"
+)
+
+var (
+	// Version is the build version.  This is set using ldflags -X
+	version = "development"
+
+	// Date is the build date.  This is set using ldflags -X
+	Date = "YYYY-MM-DD" // YYYY-MM-DD
+
+	// DSN used for sentry
+	SentryDSN string
+)
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	// Init sentry
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn: SentryDSN,
+	})
+	if err != nil {
+		log.Println("Could not load sentry.")
+	}
+	v.SetBuildInfo(version, Date, "", "")
+
+	// Setup command
+	command := commandtcv.NewCmdTCV()
+
+	if err := command.Execute(); err != nil {
+		// TODO: Handle exit codes here
+		pterm.Error.Println(err)
+		sentry.CaptureException(err)
+		sentry.Flush(2 * time.Second)
+		os.Exit(1)
+	}
+}
---
+++ command_tcv/root.go
@@ -0,0 +1,23 @@
+package commandtcv
+
+import (
+	"github.com/deepsourcelabs/cli/command/report"
+	"github.com/spf13/cobra"
+)
+
+// NewCmdTCV returns a Cobra command containing commands related to test coverage reporting.
+func NewCmdTCV() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "deepsource <command> <subcommand> [flags]",
+		Short: "DeepSource CLI",
+		Long: `Welcome to DeepSource CLI
+Now ship good code directly from the command line.
+Login into DeepSource using the command : deepsource auth login`,
+		SilenceErrors: true,
+		SilenceUsage:  true,
+	}
+
+	cmd.AddCommand(report.NewCmdReport())
+
+	return cmd
+}
