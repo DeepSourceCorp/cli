67b56b775a0d3aff1e2c14f27e0fbc2671d67381
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-07-27 15:16:53 +0530

    feat: provide Homebrew completions (#175)
    * feat: provide Homebrew completions
    
    * Update gen-completions.sh
    
    Co-authored-by: Siddhant N Trivedi <68370997+siddhant-deepsource@users.noreply.github.com>

---
+++ scripts/gen-completions.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+rm -rf completions
+mkdir completions
+
+# Generate completion using the in-built cobra completion command
+for shell in bash zsh fish; do
+  go run main.go completion "$shell" > "completions/deepsource.$shell"
+done
