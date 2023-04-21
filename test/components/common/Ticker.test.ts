5cddccd3303817ef2f0684ca2b6439d5c82285f4
Yash Dave <87606277+yash-deepsource@users.noreply.github.com>
2023-04-21 15:09:47 +0530

    test: slim down ticker tests for faster runs (#1938)

--- test/components/common/Ticker.test.ts
+++ test/components/common/Ticker.test.ts
@@ -10,28 +10,47 @@ import {
 } from '~/test/utils'
 
 const baseProps = {
-  trendHint: 'since last commit'
+  trendPositive: true,
+  showBg: false,
+  isPercent: false,
+  trendHint: 'since last commit',
+  trendDirection: 'up',
+  icon: 'triangle-up',
+  trendValue: 275
 }
 
 describe('[[ Ticker ]]', () => {
   test('renders `Ticker` with all props', () => {
     const trendPositiveOptions = generateBooleanProps('trendPositive')
-    const isPercentOptions = generateBooleanProps('isPercent')
     const showBgOptions = generateBooleanProps('showBg')
 
     const trendDirectionOptions = generateStringProps('trendDirection', ['up', 'down'])
     const iconOptions = generateStringProps('icon', ['triangle-up', 'triangle-down'])
 
+    cartesian(trendPositiveOptions, showBgOptions, trendDirectionOptions, iconOptions).forEach(
+      (propCombination) => {
+        const props = {
+          ...baseProps,
+          ...propCombination
+        }
+
+        const { html } = render(Ticker, {
+          props,
+          stubs: {
+            ZIcon: true
+          }
+        })
+        expect(html()).toMatchSnapshot(JSON.stringify(props))
+      }
+    )
+  })
+
+  test('renders `Ticker` with different value options', () => {
+    const isPercentOptions = generateBooleanProps('isPercent')
+
     const trendValueOptions = generateGenericProps('trendValue', ['275', 275, 0])
 
-    cartesian(
-      trendPositiveOptions,
-      isPercentOptions,
-      showBgOptions,
-      trendDirectionOptions,
-      iconOptions,
-      trendValueOptions
-    ).forEach((propCombination) => {
+    cartesian(isPercentOptions, trendValueOptions).forEach((propCombination) => {
       const props = {
         ...baseProps,
         ...propCombination
