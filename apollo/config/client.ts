9850f93089861ccd48c5996ed6e0c2d34e6e28eb
Parth Sharma <86726240+parth-deepsource@users.noreply.github.com>
2023-07-04 16:07:05 +0530

    chore: more console.log
    Signed-off-by: Parth Sharma <86726240+parth-deepsource@users.noreply.github.com>

--- apollo/config/client.ts
+++ apollo/config/client.ts
@@ -56,7 +56,7 @@ export default ({
   //? The following middleware handles forwarding the IP of the actual client.
   const forwardHeadersMiddleware = new ApolloLink((operation, forward) => {
     let additionalHeaders = {}
-
+    console.log("Request Headers: ", req.headers)
     if (process.server) {
       const clientIp =
         (req.headers['x-forwarded-for'] as string)?.split(',').pop() || req.socket.remoteAddress
