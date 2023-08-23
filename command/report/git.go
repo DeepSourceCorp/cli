0a77dae83e98d812afe44d0315ba278f58a8237c
Srijan Saurav <srijan@deepsource.io>
2023-08-23 15:23:23 +0530

    fix: Warn users when GitHub Actions is making a merge commit
    changes:
      - Change signature of `gitGetHead` to return a warning for GitHub Actions merge commits
      - Propagate the warning to `report` command response

--- command/report/git.go
+++ command/report/git.go
@@ -11,7 +11,7 @@ import (
 )
 
 // gitGetHead accepts a git directory and returns head commit OID / error
-func gitGetHead(workspaceDir string) (string, error) {
+func gitGetHead(workspaceDir string) (commitOID string, warning string, err error) {
 	// TRAVIS CI
 	// Get USER env variable.
 	if envUser := os.Getenv("USER"); envUser == "travis" {
@@ -20,19 +20,21 @@ func gitGetHead(workspaceDir string) (string, error) {
 		// Fetch value of pull request SHA. If this is a PR, it will return SHA of HEAD commit of the PR, else "".
 		// If prSHA is not empty, that means we got an SHA, which is HEAD. Return this.
 		if prSHA := os.Getenv("TRAVIS_PULL_REQUEST_SHA"); len(prSHA) > 0 {
-			return prSHA, nil
+			return prSHA, "", nil
 		}
 	}
 
 	// GITHUB ACTIONS
 	// Check if it is a GitHub Action Environment
-	// If it is: then get the HEAD from `GITHUB_SHA` environment
-	if _, isGitHubEnv := os.LookupEnv("GITHUB_ACTIONS"); isGitHubEnv {
+	// If it is: then get the HEAD commit from `GITHUB_SHA` environment and do an early exit
+	_, isGitHubEnv := os.LookupEnv("GITHUB_ACTIONS")
+	if isGitHubEnv {
 		// When GITHUB_REF is not set, GITHUB_SHA points to original commit.
 		// When set, it points to the "latest *merge* commit in the branch".
+		// We don't want early exit in the above case.
 		// Ref: https://help.github.com/en/actions/reference/events-that-trigger-workflows#pull-request-event-pull_request
 		if _, isBranchCommit := os.LookupEnv("GITHUB_REF"); !isBranchCommit {
-			return os.Getenv("GITHUB_SHA"), nil
+			return os.Getenv("GITHUB_SHA"), "", nil
 		}
 	}
 
@@ -47,18 +49,41 @@ func gitGetHead(workspaceDir string) (string, error) {
 	// GIT_COMMIT_SHA=$(git --no-pager rev-parse HEAD | tr -d '\n')
 	// docker run -e DEEPSOURCE_DSN -e GIT_COMMIT_SHA ...
 	if _, isManuallyInjectedSHA := os.LookupEnv("GIT_COMMIT_SHA"); isManuallyInjectedSHA {
-		return os.Getenv("GIT_COMMIT_SHA"), nil
+		return os.Getenv("GIT_COMMIT_SHA"), "", nil
 	}
 
-	// If we are here, it means this is neither GitHub Action on default branch,
-	// nor a travis env with PR. Continue to fetch the headOID via the git command.
+	// If we are here, it means that we couldn't reliably find the HEAD commit.
+	// Fetch it manually using the git command.
 	headOID, err := fetchHeadManually(workspaceDir)
 	if err != nil {
 		fmt.Println(err)
 		sentry.CaptureException(err)
-		return "", err
+		return "", "", err
 	}
-	return headOID, nil
+
+	// One last check for GitHub Actions. In the above code for GitHub Action, we did an early exit
+	// only when the GITHUB_REF wasn't set. This would be for a very small subset of GitHub events.
+	// We want to automatically detect merge commits, which pull_request events are nutorious to make.
+	// We are anyways going to return `headOID` fetched manually, but want to warn users about the merge commit.
+	if isGitHubEnv {
+		eventName := os.Getenv("GITHUB_EVENT_NAME")
+		eventCommitSha := os.Getenv("GITHUB_SHA")
+		// When ref is not provided during the checkout step, headOID would be the same as GITHUB_SHA
+		// This confirms the merge commit.
+		// event names where GITHUB_SHA would be of a merge commit:
+		//    "pull_request",
+		// 	  "pull_request_review",
+		// 	  "pull_request_review",
+		// 	  "pull_request_review_comment",
+		if strings.HasPrefix(eventName, "pull_request") && headOID == eventCommitSha {
+			warning = "Warning: Looks like the checkout step is making a merge commit. " +
+				"Test coverage Analyzer would not run for the reported artifact because the merge commit doesn't exist upstream.\n" +
+				"Please refer to the docs for required changes. Ref: https://docs.deepsource.com/docs/analyzers-test-coverage#with-github-actions"
+
+		}
+	}
+
+	return headOID, warning, nil
 }
 
 // Fetches the latest commit hash using the command `git rev-parse HEAD`
