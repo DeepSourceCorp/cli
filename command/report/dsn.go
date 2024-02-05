b4ee0fc5d67fe3315e88030d55a8aeae7e3e97d6
Vishnu Jayadevan <68588475+vishnu-deepsource@users.noreply.github.com>
2024-02-05 16:12:29 +0530

    feat: start refactoring the report command (#238)

---
+++ command/report/dsn.go
@@ -0,0 +1,27 @@
+package report
+
+import (
+	"errors"
+	"regexp"
+)
+
+var ErrInvalidDSN = errors.New("DeepSource | Error | Invalid DSN. Cross verify DEEPSOURCE_DSN value against the settings page of the repository")
+
+type DSN struct {
+	Protocol string
+	Host     string
+	Token    string
+}
+
+func NewDSN(raw string) (*DSN, error) {
+	dsnPattern := regexp.MustCompile(`^(https?)://([^:@]+)@([^:/]+(?:\:\d+)?)`)
+	matches := dsnPattern.FindStringSubmatch(raw)
+	if len(matches) != 4 {
+		return nil, ErrInvalidDSN
+	}
+	return &DSN{
+		Protocol: matches[1],
+		Token:    matches[2],
+		Host:     matches[3],
+	}, nil
+}
