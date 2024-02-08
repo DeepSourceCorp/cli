4cd70cbf9c892775c5383f4b45e4a06ac539d235
Vishnu Jayadevan <vishnu@deepsource.io>
2024-02-08 12:49:06 +0530

    fix: make it possible to skip transformer selection while generating config

--- command/config/generate/transformers_input.go
+++ command/config/generate/transformers_input.go
@@ -9,9 +9,16 @@ func (o *Options) collectTransformersInput() (err error) {
 	transformerPromptMsg := "Would you like to activate any Transformers for any languages?"
 	transformerPromptHelpText := "DeepSource Transformers automatically help to achieve auto-formatting of code. Add a transformer by selecting the code formatting tool of your choice."
 
-	o.ActivatedTransformers, err = utils.SelectFromMultipleOptions(transformerPromptMsg, transformerPromptHelpText, utils.TransformersData.TransformerNames)
+	response, err := utils.ConfirmFromUser(transformerPromptMsg, transformerPromptHelpText)
 	if err != nil {
 		return err
+	}
+
+	if response {
+		o.ActivatedTransformers, err = utils.SelectFromMultipleOptions("Which transformers would you like to use?", "You can select multiple transformers at the same time", utils.TransformersData.TransformerNames)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
